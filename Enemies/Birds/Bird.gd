d56c8da98c05511a4e5f19e7a91618700ef2b4de
indisofyar <38954340+indisofyar@users.noreply.github.com>
2025-12-31 12:09:27 +0000

    Started

--- Enemies/Birds/Bird.gd
+++ Enemies/Birds/Bird.gd
@@ -3,9 +3,9 @@ extends "res://Enemies/Obstacle.gd"
 @onready var clipping_area: CollisionShape2D = $DetectionArea/CollisionShape2D
 
 enum {
-    BACK,
-    TOP,
-    BOTTOM,
+	BACK,
+	TOP,
+	BOTTOM,
 }
 
 var spawnable_locations: Array[int] = [TOP, BACK, BOTTOM]
@@ -16,75 +16,75 @@ var members_remaining_to_spawn: int = 0
 
 # Flock code
 func _further_setup() -> void: ## Extension of _ready() to implement extra logic per class of obstacle
-    var rand_num = randf()
+	var rand_num = randf()
 
-    if current_role == ROLES.LEADER and rand_num < SpawnManager.current_bird_flock_probability:
-        initial_leader_pos = self.position
+	if current_role == ROLES.LEADER and rand_num < SpawnManager.current_bird_flock_probability:
+		initial_leader_pos = self.position
 
-        total_flock_size = randi_range(SpawnManager.current_bird_flock_size.x, SpawnManager.current_bird_flock_size.y)
+		total_flock_size = randi_range(SpawnManager.current_bird_flock_size.x, SpawnManager.current_bird_flock_size.y)
 
-        members_remaining_to_spawn = total_flock_size
-        _spawn_flock(members_remaining_to_spawn)
-    if current_role == ROLES.COLEADER and members_remaining_to_spawn > 0:
-        _spawn_flock(members_remaining_to_spawn)
+		members_remaining_to_spawn = total_flock_size
+		_spawn_flock(members_remaining_to_spawn)
+	if current_role == ROLES.COLEADER and members_remaining_to_spawn > 0:
+		_spawn_flock(members_remaining_to_spawn)
 
 func _spawn_flock(members_to_spawn: int):
-    var members_spawned: int
+	var members_spawned: int
 
-    if current_role == ROLES.COLEADER:
-        members_spawned = 0
+	if current_role == ROLES.COLEADER:
+		members_spawned = 0
 
-        if members_to_spawn > 3: # Limiting number of members under a single leader/co-leader
-            members_to_spawn = 3
+		if members_to_spawn > 3: # Limiting number of members under a single leader/co-leader
+			members_to_spawn = 3
 
-    elif current_role == ROLES.LEADER:
-        members_spawned = 1
+	elif current_role == ROLES.LEADER:
+		members_spawned = 1
 
-        if members_to_spawn > 4: # Limiting number of members under a single leader/co-leader
-            members_to_spawn = 4
+		if members_to_spawn > 4: # Limiting number of members under a single leader/co-leader
+			members_to_spawn = 4
 
 
-    members_remaining_to_spawn -= members_to_spawn
+	members_remaining_to_spawn -= members_to_spawn
 
-    while members_spawned < members_to_spawn:
-        var new_individual: Area2D = self.duplicate()
-        var new_spawn_location: int = spawnable_locations.pick_random()
+	while members_spawned < members_to_spawn:
+		var new_individual: Area2D = self.duplicate()
+		var new_spawn_location: int = spawnable_locations.pick_random()
 
-        spawnable_locations.erase(new_spawn_location)
-        new_individual.position.x = self.position.x + (-1 * clipping_area.shape.radius )
+		spawnable_locations.erase(new_spawn_location)
+		new_individual.position.x = self.position.x + (-1 * clipping_area.shape.radius )
 
-        if new_spawn_location == BACK:
-            new_individual.position.x += (-1 * clipping_area.shape.radius)
+		if new_spawn_location == BACK:
+			new_individual.position.x += (-1 * clipping_area.shape.radius)
 
-            if members_remaining_to_spawn > 0:
-                new_individual = _transfer_flock_properties(new_individual, ROLES.COLEADER)
-            else:
-                new_individual = _transfer_flock_properties(new_individual, ROLES.MEMBER)
+			if members_remaining_to_spawn > 0:
+				new_individual = _transfer_flock_properties(new_individual, ROLES.COLEADER)
+			else:
+				new_individual = _transfer_flock_properties(new_individual, ROLES.MEMBER)
 
-        else:
-            match new_spawn_location:
-                TOP:
-                    new_individual.position.y += -1 * clipping_area.shape.radius
-                BOTTOM:
-                    new_individual.position.y += clipping_area.shape.radius
+		else:
+			match new_spawn_location:
+				TOP:
+					new_individual.position.y += -1 * clipping_area.shape.radius
+				BOTTOM:
+					new_individual.position.y += clipping_area.shape.radius
 
-            new_individual = _transfer_flock_properties(new_individual, ROLES.MEMBER)
+			new_individual = _transfer_flock_properties(new_individual, ROLES.MEMBER)
 
-        get_parent().add_child(new_individual)
-        members_spawned += 1
+		get_parent().add_child(new_individual)
+		members_spawned += 1
 
 func _transfer_flock_properties(new_member: Node, role_of_individual: int = ROLES.MEMBER) -> Node:
-    new_member.current_role = role_of_individual
-    new_member.flock_speed = flock_speed
-    new_member.initial_leader_pos = initial_leader_pos
+	new_member.current_role = role_of_individual
+	new_member.flock_speed = flock_speed
+	new_member.initial_leader_pos = initial_leader_pos
 
-    if role_of_individual == ROLES.COLEADER:
-        new_member.members_remaining_to_spawn = members_remaining_to_spawn
+	if role_of_individual == ROLES.COLEADER:
+		new_member.members_remaining_to_spawn = members_remaining_to_spawn
 
-    return new_member
+	return new_member
 
 func _after_hit():
-    UiManager.emit_signal("triggered_screen_shake", UiManager.STRENGTH_TYPES.LOW)
-    self.horizontal_speed = 0
-    animated_sprite.flip_v = true
-    animated_sprite.stop()
+	UiManager.emit_signal("triggered_screen_shake", UiManager.STRENGTH_TYPES.LOW)
+	self.horizontal_speed = 0
+	animated_sprite.flip_v = true
+	animated_sprite.stop()
--- Managers/ui_manager.gd
+++ Managers/ui_manager.gd
@@ -31,67 +31,67 @@ signal time_phase_changed(current_time_phase: int)
 signal triggered_screen_shake(shake_type: int)
 
 enum TIME_PHASES {
-    DAY,
-    NIGHT
+	DAY,
+	NIGHT
 }
 
 enum BACKGROUND_TYPES {
-    RANDOM,
-    TIME_BASED,
-    DAY,
-    NIGHT
+	RANDOM,
+	TIME_BASED,
+	DAY,
+	NIGHT
 }
 
 enum CONTROL_TYPES {
-    GUIDE,
-    FOLLOW
+	GUIDE,
+	FOLLOW
 }
 
 enum DIRECTIONS {
-    LEFT = -1,
-    RESET,
-    RIGHT
+	LEFT = -1,
+	RESET,
+	RIGHT
 }
 
 # Related to screen shake
 enum STRENGTH_TYPES {
-    HIGH,
-    MED,
-    LOW
+	HIGH,
+	MED,
+	LOW
 }
 
 const SHAKE_STRENGTHS = {
-    STRENGTH_TYPES.HIGH: 0.5,
-    STRENGTH_TYPES.MED: 0.25,
-    STRENGTH_TYPES.LOW: 0.2
+	STRENGTH_TYPES.HIGH: 0.5,
+	STRENGTH_TYPES.MED: 0.25,
+	STRENGTH_TYPES.LOW: 0.2
 }
 
 #MENU_LEVEL.MAIN is index 1 not zero so keep that in mind if you change to an array
 enum MENU_IDS {
-        NONE,
-        MAIN_MENU,
-        START_MENU,
-        SETTINGS,
-        CREDITS,
-        CONTROLS,
-        INGAME_UI,
-        GAME_OVER,
-        SKIN_SELECTOR,
-        STATISTICS,
-        GUIDE
-    }
+		NONE,
+		MAIN_MENU,
+		START_MENU,
+		SETTINGS,
+		CREDITS,
+		CONTROLS,
+		INGAME_UI,
+		GAME_OVER,
+		SKIN_SELECTOR,
+		STATISTICS,
+		GUIDE
+	}
 
 var menus = {
-    MENU_IDS.MAIN_MENU : preload("res://UI/main_menu.tscn").instantiate(),
-    MENU_IDS.START_MENU: preload("res://UI/start_menu.tscn").instantiate(),
-    MENU_IDS.SETTINGS : preload("res://UI/settings.tscn").instantiate(),
-    MENU_IDS.CREDITS : preload("res://UI/credits.tscn").instantiate(),
-    MENU_IDS.INGAME_UI : preload("res://UI/ingame_ui.tscn").instantiate(),
-    MENU_IDS.GAME_OVER : preload("res://UI/game_over.tscn").instantiate(),
-    MENU_IDS.CONTROLS: preload("res://UI/controls.tscn").instantiate(),
-    MENU_IDS.SKIN_SELECTOR: preload("res://UI/skin_selector.tscn").instantiate(),
-    MENU_IDS.STATISTICS: preload("res://UI/statistics.tscn").instantiate(),
-    MENU_IDS.GUIDE: preload("res://UI/guide.tscn").instantiate(),
+	MENU_IDS.MAIN_MENU : preload("res://UI/main_menu.tscn").instantiate(),
+	MENU_IDS.START_MENU: preload("res://UI/start_menu.tscn").instantiate(),
+	MENU_IDS.SETTINGS : preload("res://UI/settings.tscn").instantiate(),
+	MENU_IDS.CREDITS : preload("res://UI/credits.tscn").instantiate(),
+	MENU_IDS.INGAME_UI : preload("res://UI/ingame_ui.tscn").instantiate(),
+	MENU_IDS.GAME_OVER : preload("res://UI/game_over.tscn").instantiate(),
+	MENU_IDS.CONTROLS: preload("res://UI/controls.tscn").instantiate(),
+	MENU_IDS.SKIN_SELECTOR: preload("res://UI/skin_selector.tscn").instantiate(),
+	MENU_IDS.STATISTICS: preload("res://UI/statistics.tscn").instantiate(),
+	MENU_IDS.GUIDE: preload("res://UI/guide.tscn").instantiate(),
 }
 
 var need_basic_tutorial: bool = false
@@ -105,81 +105,80 @@ var current_control_type: int = CONTROL_TYPES.GUIDE
 var star_count_global_pos: Vector2 # To animate star collection
 
 func _ready() -> void:
-    self.first_startup.connect(func () -> void: need_basic_tutorial = true)
-    GameManager.game_started.connect(_check_tutorial_need)
-    DataManager.data_reloaded.connect(_reload_data)
-    DataManager.data_got_changed.connect(_reload_data)
+	self.first_startup.connect(func () -> void: need_basic_tutorial = true)
+	GameManager.game_started.connect(_check_tutorial_need)
+	DataManager.data_reloaded.connect(_reload_data)
+	DataManager.data_got_changed.connect(_reload_data)
 
-    self.skipped_to_main_menu.connect(spawn_menu.bind(MENU_IDS.MAIN_MENU))
-    self.opened_start_menu.connect(spawn_menu.bind(MENU_IDS.START_MENU))
-    self.opened_settings.connect(spawn_menu.bind(MENU_IDS.SETTINGS))
-    self.opened_credits.connect(spawn_menu.bind(MENU_IDS.CREDITS))
-    self.opened_skin_selector.connect(spawn_menu.bind(MENU_IDS.SKIN_SELECTOR))
-    self.opened_statistics.connect(spawn_menu.bind(MENU_IDS.STATISTICS))
-    self.opened_guide.connect(spawn_menu.bind(MENU_IDS.GUIDE))
+	self.skipped_to_main_menu.connect(spawn_menu.bind(MENU_IDS.MAIN_MENU))
+	self.opened_start_menu.connect(spawn_menu.bind(MENU_IDS.START_MENU))
+	self.opened_settings.connect(spawn_menu.bind(MENU_IDS.SETTINGS))
+	self.opened_credits.connect(spawn_menu.bind(MENU_IDS.CREDITS))
+	self.opened_skin_selector.connect(spawn_menu.bind(MENU_IDS.SKIN_SELECTOR))
+	self.opened_statistics.connect(spawn_menu.bind(MENU_IDS.STATISTICS))
+	self.opened_guide.connect(spawn_menu.bind(MENU_IDS.GUIDE))
 
-    self.triggered_gamearea_setup.connect(_spawn_ingame_uis)
-    self.triggered_menu_ui_setup.connect(spawn_menu.bind(MENU_IDS.GAME_OVER))
+	self.triggered_gamearea_setup.connect(_spawn_ingame_uis)
+	self.triggered_menu_ui_setup.connect(spawn_menu.bind(MENU_IDS.GAME_OVER))
 
 
 func _reload_data() -> void:
-    current_control_type = DataManager.settings.control_type
+	current_control_type = DataManager.settings.control_type
 
-    if DataManager.statistics.powerups_used < 1 and not need_basic_tutorial:
-        need_powerup_tutorial = true
+	if DataManager.statistics.powerups_used < 1 and not need_basic_tutorial:
+		need_powerup_tutorial = true
 
 
 func spawn_menu(menu_id: int):
-    if menu_id == MENU_IDS.GAME_OVER:
-        await _wait_till_transition()
-        GameManager.emit_signal("game_over")
-        self.emit_signal("remove_transition")
+	if menu_id == MENU_IDS.GAME_OVER:
+		await _wait_till_transition()
+		GameManager.emit_signal("game_over")
+		self.emit_signal("remove_transition")
 
-    call_deferred("_deferred_load_menu", menu_id)
+	call_deferred("_deferred_load_menu", menu_id)
 
 func _spawn_ingame_uis():
-    await _wait_till_transition()
+	await _wait_till_transition()
 
-    main_scene.add_child(menus[MENU_IDS.CONTROLS])
-    spawn_menu(MENU_IDS.INGAME_UI)
+	main_scene.add_child(menus[MENU_IDS.CONTROLS])
+	spawn_menu(MENU_IDS.INGAME_UI)
 
-    GameManager.call_deferred("emit_signal","game_started")
+	GameManager.call_deferred("emit_signal","game_started")
 
 func _deferred_load_menu(menu_id: int):
-    if not main_scene:
-        push_error("Main scene not set in UiManager")
-    #replace the current menus instance with the new ones
-    current_menu = menus[menu_id]
+	if not main_scene:
+		push_error("Main scene not set in UiManager")
+	#replace the current menus instance with the new ones
+	current_menu = menus[menu_id]
 
-    var menu_container = main_scene.find_child("Menu", false, false)
+	var menu_container = main_scene.find_child("Menu", false, false)
 
-    if not menu_container:
-        var menu_node = Node.new()
-        menu_node.set_name("Menu")
-        main_scene.add_child(menu_node)
-        menu_container = menu_node
+	if not menu_container:
+		var menu_node = Node.new()
+		menu_node.set_name("Menu")
+		main_scene.add_child(menu_node)
+		menu_container = menu_node
 
-    #clear the current menu item/s
-    for location in menu_container.get_children():
-        menu_container.remove_child(location)
+	#clear the current menu item/s
+	for location in menu_container.get_children():
+		menu_container.remove_child(location)
 
-    #add our selected menu
-    menu_container.add_child(current_menu)
+	#add our selected menu
+	menu_container.add_child(current_menu)
 
-    emit_signal("remove_transition")
+	emit_signal("remove_transition")
 
 func _check_tutorial_need() -> void:
-    if need_basic_tutorial:
-        emit_signal("show_basic_tutorial")
-        need_basic_tutorial = false
-    else:
-        GameManager.emit_signal("start_spawning")
+	if need_basic_tutorial:
+		emit_signal("show_basic_tutorial")
+		need_basic_tutorial = false
+	else:
+		GameManager.emit_signal("start_spawning")
 
-        if need_powerup_tutorial:
-            emit_signal("show_powerup_tutorial")
+		if need_powerup_tutorial:
+			emit_signal("show_powerup_tutorial")
 
 func _wait_till_transition() -> void:
-    emit_signal("show_transition")
+	emit_signal("show_transition")
 
-    await self.entered_transition
-
+	await self.entered_transition
